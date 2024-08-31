87bea88bdeb36e8fdbbecbec3df455a61f406cce
Haroon Irfan <haroon.irfan@elev.ga.ntig.se>
2024-08-31 13:19:12 +0200

    Interaktiv 1.2

--- Interaktiv/obj/Debug/net8.0/Interaktiv.AssemblyInfo.cs
+++ Interaktiv/obj/Debug/net8.0/Interaktiv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc0312793271137b80a58ee325414a3db0b7165f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b452959b57ae102856f40602b4ddaaecc1c142")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
